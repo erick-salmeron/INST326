1ef98bd9996cbf4e2a54350945e70bf895b4fe63
Erick Salmeron <ericksalmeron00@gmail.com>
2023-03-31 23:48:04 -0400

    Second Commit

--- python script.py
+++ python script.py
@@ -1,2 +1,12 @@
 #First commit to this repositoy for Exercise 4-
 print("Hello, World!")
+
+
+"""
+These comments will serve to show just making updates to this and and submitting them.
+
+It is during our darkest moments that we must focus to see the light. -Aristotle
+
+
+"""
+
